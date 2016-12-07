e752c8ac52184f8e79529af701be6d6ffaa9d5cd
Tobias L. Maier <me@tobiasmaier.info>
2016-12-07 23:33:22 +0100

    Fix comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# Prepend "htmlproofer" if the first argument is not an executable
+# Prepend "stout" if the first argument is not an executable
 if ! type -- "$1" &> /dev/null; then
 	set -- stout "$@"
 fi
